51ea483b22bd3d4e7f055588c1440387ba51cfbb
Ronnakorn <ronnakorn.fahkiat@outlook.com>
2023-10-28 17:08:15 +0700

    setup prisma mdel

---
+++ src/app.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const express = require('express')
+const cors = require('cors');
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+const PORT  = process.env.PORT || '2000';
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on ${PORT}`)
+})
