467ab2535efa7c43a36614492f08ac36b62104ad
TimeRuchutrakool <timeruchu0808@gmail.com>
2023-10-31 11:02:46 +0700

    to fix negative amount

--- src/controllers/cart-controller.js
+++ src/controllers/cart-controller.js
@@ -1,82 +1,64 @@
 const prisma = require("../models/prisma");
 const createError = require("../utils/create-error");
-const {removeItemInCartSchema} = require('../validators/cart-validators')
+const { removeItemInCartSchema } = require("../validators/cart-validators");
 
 exports.addCartItems = async (req, res, next) => {
   try {
     const { id: userId } = req.user;
     const { productId, amount } = req.body;
 
-    const cartItem = await prisma.cartItem.findFirst({
+    let cartItem = await prisma.cartItem.findFirst({
       where: {
-        buyerId: +userId,
+        buyerId: userId,
         productId: +productId,
       },
     });
 
     if (!cartItem) {
-      const createdCartItem = await prisma.cartItem.create({
+      cartItem = await prisma.cartItem.create({
         data: {
           productId: productId,
           amount: amount,
           buyerId: userId,
         },
       });
-      return res.json({ createdCartItem });
-    }
-
-    const updatedCartItem = await prisma.cartItem.update({
-      where: {
-        id: cartItem.id,
-      },
-      data: {
-        amount: {
-          increment: amount,
+    } else {
+      cartItem = await prisma.cartItem.update({
+        where: {
+          id: cartItem.id,
         },
-      },
-    });
-
-    // const product = await prisma.cartItem.findMany({
-    //         where : {
-    //             buyerId : +userId
-    //         },
-    //         include : {
-    //             product : {
-    //                 where : {
-    //                     productId : productId
-    //                 }
-    //             }
-    //         }
-    // })
-
-    res.status(200).json({updatedCartItem });
+        data: {
+          amount: {
+            increment: amount,
+          },
+        },
+      });
+    }
+    res.status(200).json({ cartItem });
   } catch (error) {
     console.log(error);
     next(error);
   }
 };
 
-
 //////////////////////////// Remove Product from Cart //////////////////////////////
-exports.removeItem = async (req,res,next)=>{
+exports.removeItem = async (req, res, next) => {
   try {
-    const {value , error} = removeItemInCartSchema.validate(req.params)
+    const { value, error } = removeItemInCartSchema.validate(req.params);
     // const removeItem = req.params.removeItem
-    const {id:userId} = req.user
-    if(error)
-    {
-      return next(createError('Can not remove Item',400))
+    const { id: userId } = req.user;
+    if (error) {
+      return next(createError("Can not remove Item", 400));
     }
 
-
     await prisma.cartItem.deleteMany({
-      where : {
+      where: {
         buyerId: userId,
         productId: +value.removeItem,
-      }
-    })
-    res.status(200).json({message : "Remove Success"})
+      },
+    });
+    res.status(200).json({ message: "Remove Success" });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
