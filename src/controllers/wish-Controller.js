6957d45f7bdc357179bee600c8666184757dac17
Ronnakorn <ronnakorn.fahkiat@outlook.com>
2023-11-01 11:42:21 +0700

    edit feature getCart and wish

--- src/controllers/wish-Controller.js
+++ src/controllers/wish-Controller.js
@@ -1,70 +1,84 @@
-const prisma = require('../models/prisma');
-const createError =require('../utils/create-error');
-const { addWishSchema, removeWishSchema } = require('../validators/wish-validator');
+const prisma = require("../models/prisma");
+const createError = require("../utils/create-error");
+const {
+  addWishSchema,
+  removeWishSchema,
+} = require("../validators/wish-validator");
 
+exports.getWish = async (req, res, next) => {
+  try {
+    const { id: userId } = req.user;
 
-exports.getWish = async (req,res,next)=> {
-    try {
-        const {id : userId} = req.user
-
-        const getWish = await prisma.wishItem.findMany({
-            where : {
-                buyerId : userId
+    const getWish = await prisma.wishItem.findMany({
+      where: {
+        buyerId: userId,
+      },
+      include: {
+        product: {
+          select: {
+            name: true,
+            price: true,
+            ProductImage: {
+              select: {
+                imageUrl: true,
+              },
             },
-            include : {
-                product : true
-            }
-        })
+          },
+        },
+      },
+    });
+    const products = getWish.map((product) => {
+      return {
+        id:product.product.productId,
+        name : product.product.name,
+        price : product.product.price,
+        imageUrl : product.product.ProductImage[0].imageUrl
+      };
+    });
 
-        res.status(200).json({getWish})
-
-    } catch (error) {
-        next(error)
-    }
-}
+    res.status(200).json({ products });
+  } catch (error) {
+    next(error);
+  }
+};
 
 // เพิ่มรายการโปรด
-exports.addWish = async (req,res,next)=>{
-    try {
-        const {id : userId} = req.user
-       const {value , error} = addWishSchema.validate(req.params)
-       
-        if(error)
-        {
-            next(error)
-        }
+exports.addWish = async (req, res, next) => {
+  try {
+    const { id: userId } = req.user;
+    const { value, error } = addWishSchema.validate(req.params);
 
-        // เช็คว่ามี รายการโปรด หรือยัง
-        let wishItem = await prisma.wishItem.findFirst({
-            where : {
-                buyerId : userId,
-                productId : +value.addWish
-            }
-        });
-
-        //ถ้ายังไม่มี ให้ cerate รายการโปรด
-        if(!wishItem)
-        {
-            wishItem = await prisma.wishItem.create({
-                data : {
-                    buyerId : userId,
-                    productId : +value.addWish
-                }
-            })
-            res.status(200).json({wishItem})
-        }
-        
-        else{
-         await prisma.wishItem.deleteMany({
-                where : {
-                    buyerId : userId,
-                    productId : +value.addWish
-                }
-            })
-            res.status(200).json({message : 'remove wish success'})
-        }
-       
-    } catch (error) {
-        next(error)
+    if (error) {
+      next(error);
     }
-}
+
+    // เช็คว่ามี รายการโปรด หรือยัง
+    let wishItem = await prisma.wishItem.findFirst({
+      where: {
+        buyerId: userId,
+        productId: +value.addWish,
+      },
+    });
+
+    //ถ้ายังไม่มี ให้ cerate รายการโปรด
+    if (!wishItem) {
+      wishItem = await prisma.wishItem.create({
+        data: {
+          buyerId: userId,
+          productId: +value.addWish,
+        },
+      });
+      res.status(200).json({ wishItem });
+    } else {
+      await prisma.wishItem.deleteMany({
+        where: {
+          buyerId: userId,
+          productId: +value.addWish,
+        },
+      });
+      res.status(200).json({ message: "remove wish success" });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
