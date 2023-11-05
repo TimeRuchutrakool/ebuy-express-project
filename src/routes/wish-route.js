7f7231c24bca41e2e96aea3755f50b45c7f836d9
TimeRuchutrakool <timeruchu0808@gmail.com>
2023-11-05 23:05:04 +0700

    toggle wish done

--- src/routes/wish-route.js
+++ src/routes/wish-route.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const authenticatedMiddleware = require('../middlewares/authenticates');
-const wishController = require('../controllers/wish-Controller')
+const express = require("express");
+const authenticatedMiddleware = require("../middlewares/authenticates");
+const wishController = require("../controllers/wish-Controller");
 const router = express.Router();
 
-
-router.get("/",authenticatedMiddleware,wishController.getWish);
-router.post("/:productId",authenticatedMiddleware,wishController.addWish);
+router.get("/", authenticatedMiddleware, wishController.getWish);
+router.post("/toggleWish/:productId", authenticatedMiddleware, wishController.toggleWish);
+router.get("/isWish", authenticatedMiddleware, wishController.isWish);
 
 module.exports = router;
